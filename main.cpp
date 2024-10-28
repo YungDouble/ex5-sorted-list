2c2cde42dfa34e8f93b96ff28d5848c141654e35
ezkl <therealdavos@gmail.com>
2024-10-27 21:41:13 -0500

    Added compareSortedStructures function

--- main.cpp
+++ main.cpp
@@ -21,3 +21,25 @@ int main() {
 
     return 0;
 }
+
+void compareSortedStructures(const SortedList& sortedLust, const SortedArray& sortedArray, int size) {
+    // Created pointer to iterate through the list and array
+    SortedList::Node* listNode = sortedList.head;
+
+    for (int i = 0; i < size; i++) {
+        // Ensure that the list node is not null and index is within array size
+        if (listNode == nullptr || i >= sortedArray.size) {
+              std::cerr << "Warning: Mismatch is size between list and array." << std::endl;
+              return;
+        }
+
+        if (listNode->data == sortedArray.array[i]) {
+              std::cout << "Value at index " << i << ": " << listNode->data << std::endl;
+        } else {
+            std::cerr << "Warning: Mismatch at index " << i << " - List value: " << listNode->data
+                      << ", Array Value: " << sortedArray.array[i] << std::endl;
+            return;
+        }
+          listNode = listNode->next;
+    }
+}
